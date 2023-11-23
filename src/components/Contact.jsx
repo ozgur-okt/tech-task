24d3a4e4cf7735132220091426fcf45e7a642a14
Özgür Oktay <ozgur@Ozgur-MacBook-Air.local>
2023-11-23 21:58:25 +0300

    contact form responsive development

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,8 +6,10 @@ function Contact() {
     <div className={styles.contactContainer}>
       <h5>{contact.section}</h5>
       <h1>{contact.title}</h1>
-      <input className={styles.email} type={contact.inputs.email.type} placeholder={contact.inputs.email.placeholder} />
-      <input className={styles.subject} type={contact.inputs.subject.type} placeholder={contact.inputs.subject.placeholder} />
+      <div className={styles.inputs}>
+        <input className={styles.email} type={contact.inputs.email.type} placeholder={contact.inputs.email.placeholder} />
+        <input className={styles.subject} type={contact.inputs.subject.type} placeholder={contact.inputs.subject.placeholder} />
+      </div>
       <textarea className={styles.message} type={contact.inputs.message.type} placeholder={contact.inputs.message.placeholder} />
       <button>{contact.buttonText}</button>
     </div>
